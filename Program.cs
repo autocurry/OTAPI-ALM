81103ae88d27c02788f057ba96ab933f1495e709
sandeep <sandeepsekhar555@gmail.com>
2018-11-24 22:55:39 +0800

    updated program file

--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@ namespace OTAPI_ALM
             public static IConfiguration _configuration {get;set;}
         public static void Main(string[] args)
         {
-     
+            
          var builder = new ConfigurationBuilder()
-      .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)
-      .SetBasePath(Directory.GetCurrentDirectory())
-      .AddEnvironmentVariables();
+                         .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddEnvironmentVariables();
 
       _configuration = builder.Build();
 
       var value = _configuration["Message"];
+      
         }
     }
 }
